c1679327248e8b9db7dab800449d9adfb0064418
Anan014d <22bec014@iiitdmj.ac.in>
2025-05-05 18:30:43 +0530

    first commit

---
+++ Frontend/src/components/Hamburger.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './Hamburger.css';
+import { Link } from 'react-router-dom';
+
+
+const Hamburger = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="hamburger-menu">
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className={menuOpen ? "hline hline1" : "hline"}></div>
+        <div className={menuOpen ? "hline hline2" : "hline"}></div>
+        <div className={menuOpen ? "hline hline3" : "hline"}></div>
+      </div>
+      <div className={`slide-menu ${menuOpen ? 'open' : ''}`}>
+        <button className="menu-button-open" onClick={toggleMenu}>🡠</button>
+        <div className="links-container">
+          <Link to="/AddRoute" className="nav-link">Add route</Link>
+          <Link to="/ViewRoutes" className="nav-link">Routes</Link>
+          <Link to="/VehicleData" className="nav-link">Vehicles</Link>
+          <Link to="/DriverData" className="nav-link">Drivers</Link>
+          <Link to="/alerts" className="nav-link">Alerts</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hamburger;
